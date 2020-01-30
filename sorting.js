d770c3caeac564e15801cf2883d93e6dc59e94b0
Kayla Melton <kannmelton2@gmail.com>
2020-01-29 19:47:06 -0600

    work in progress - added cards, jumbotron, form, some styling, needs function

--- sorting.js
+++ sorting.js
@@ -1,1 +1,43 @@
-console.log('sorting');
+const students = [
+    {
+        name: 'Sarah Potter',
+        house: 'Ravenclaw',
+        expelled: false,
+        uniqueID: ''
+    }
+];
+
+// PRINT TO DOM FUNCTION
+const printToDom = (containerId, textToPrint) => {
+    const selectedDiv = document.getElementById(containerId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+// CREATE CARDS FUNCTION
+const buildCards = (containerId, arr) => {
+    let domStr = '';
+    for (let i = 0; i < arr.length; i++) {
+        domStr += '<div class="col-md-6 col-lg-4>"';
+        domStr += '   <div class="card">';
+        domStr += '     <div class="card-body border border-light bg-light mb-3 text-center">';
+        domStr += `         <h5 class="card-title">${arr[i].name}</h5>`;
+        domStr += `         <p class="card-text">${arr[i].house}</p>`;
+        domStr += '         <a href="#" class="btn btn-danger">Expel</a>';
+        domStr += '     </div>';
+        domStr += '   </div>';
+        domStr += '</div>';
+    }
+    printToDom(containerId, domStr);
+};
+
+// EVENT LISTENERS FUNCTION
+const events = () => {
+    document.getElementById('replacethis').addEventListener('click', functionName);
+}
+
+// INIT FUNCTION - RUNS ON OPEN
+const init = () => {
+    buildCards('studentCards', students);
+}
+
+init();
